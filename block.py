d49f1a52ee146e12d59299949e0d2b0f1cbc2750
lvxiaoyu <polako@163.com>
2018-08-23 22:45:35 +0800

    block结构

--- block.py
+++ block.py
@@ -1,18 +1,27 @@
 import hashlib
 import json
+import threading
+
+from datetime import datetime
+
+from net import broadcast_block
+
+
+def calculate_hash(index, pre_hash, timestamp, data):
+    thestring = str(index) + pre_hash + str(timestamp) + json.dumps(data)
+    return hashlib.sha256(thestring.encode()).hexdigest()
 
 
 class Block(object):
-    def __init__(self, index, timestamp, pre_hash, data):
+    def __init__(self, index, pre_hash, data):
         self.index = index
-        self.timestamp = timestamp
+        self.timestamp = int(datetime.now().timestamp())
         self.pre_hash = pre_hash
         self.data = data
-        self.hash = self.calculate_hash()
+        self.hash = calculate_hash(index, pre_hash, self.timestamp, data)  # 在这里就计算hash意味着区块一经创建就不能更改
 
-    def calculate_hash(self):
-        thestring = str(self.index) + self.pre_hash + self.timestamp + json.dumps(self.data)
-        return hashlib.sha256(thestring.encode()).hexdigest()
+    def refresh_hash(self):
+        self.hash = calculate_hash(self.index, self.pre_hash, self.timestamp, self.data)
 
     def write_to_disk(self):
         pass
@@ -21,21 +30,45 @@ class Block(object):
         self.data = disk_data
 
 
-def create_block():
-    pass
+def verify_block(blk):
+    if blk is isinstance(Block):
+        return blk.hash == calculate_hash(blk.index, blk.pre_hash, blk.timestamp, blk.data)
+    return False
 
 
-def broadcast_block():
-    pass
+class _BlockChain(object):
+    _thread_lock = threading.Lock()
+
+    @staticmethod
+    def process_instance():
+        """
+        进程内单例
+        """
+        if not hasattr(_BlockChain, '_instance'):
+            with _BlockChain._thread_lock:
+                if not hasattr(_BlockChain, '_instance'):
+                    _BlockChain._instance = _BlockChain()
+        return _BlockChain._instance
+
+    def __init__(self):
+        """
+        1. 从配置文件配置的数据目录中，加载现有的区块链数据，校验并加载到内存里面
+        """
+        self.chain = []
+        pass
+
+    def add_block(self, blk):
+        if verify_block(blk):
+            if blk.index == len(self.chain):
+                blk.pre_hash = self.chain[-1].hash
+                blk.refresh_hash()
+                self.chain.append(blk)
+                broadcast_block(blk)
+                return True
+        return False
 
 
-def verify_block():
-    pass
+BlockChain = _BlockChain.process_instance()
 
 
-def add_trans_to_block():
-    pass
 
-
-def add_block():
-    pass
--- net.py
+++ net.py
@@ -1,3 +1,28 @@
+from enum import Enum
+
+
+class EnumMsgType(Enum):
+    NEW_BLK = 1
+
+
+class Node(object):
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+
+    def notify(self, mtype, blk):
+        # 调用异步IO与远程节点连接并通知
+        pass
+
+
+ALL_NODES = []
+
+
+def broadcast_block(blk):
+    for node in ALL_NODES:
+        node.notify(EnumMsgType.NEW_BLK, blk)
+
+
 def update_nodes():
     pass
 
